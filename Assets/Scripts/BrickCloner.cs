f3f34bfce8c6dc4a48324f7ddf8387289a750e3e
IanPol0 <ipolonuer@gmail.com>
2022-05-17 15:55:31 -0300

    clase

---
+++ Assets/Scripts/BrickCloner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public float xOffset;
+    int counter=0;
+    public Text UserOffset;
+
+    public void cloneBrick()
+    {
+        if (counter == 0)
+        {
+            float.Parse(UserOffset.text);
+        }
+        if (counter < 5){
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            Destroy(clon, 2);
+            clon.transform.position = new Vector3(xOffset, 0, 0);
+            xOffset += 1.1f;
+            counter++;
+        }
+        
+    }
+}
